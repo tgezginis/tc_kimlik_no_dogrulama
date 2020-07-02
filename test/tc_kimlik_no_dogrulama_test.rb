bf23dec6b8ac492926bd49ffdc244586a74b73ff
Tolga Gezginiş <tolga@gezginis.com>
2020-07-02 19:38:20 +0300

    Update spec

--- test/tc_kimlik_no_dogrulama_test.rb
+++ test/tc_kimlik_no_dogrulama_test.rb
@@ -5,15 +5,15 @@ require 'tc_kimlik_no_dogrulama'
 
 class TcKimlikNoDogrulamaTest < Minitest::Test
   def test_success
-    assert_equal true, TcKimlikNoDogrulama.verify(18_071_470_110)
-    assert_equal true, TcKimlikNoDogrulama.verify(74_091_671_050)
-    assert_equal true, TcKimlikNoDogrulama.verify(68_461_383_032)
+    assert_equal true, TcKimlikNoDogrulama.verify(18071470110)
+    assert_equal true, TcKimlikNoDogrulama.verify(74091671050)
+    assert_equal true, TcKimlikNoDogrulama.verify(68461383032)
   end
 
   def test_fail
     assert_equal false, TcKimlikNoDogrulama.verify('01234567890')
     assert_equal false, TcKimlikNoDogrulama.verify('string')
-    assert_equal false, TcKimlikNoDogrulama.verify(30_402_050_302)
+    assert_equal false, TcKimlikNoDogrulama.verify(30402050302)
     assert_equal false, TcKimlikNoDogrulama.verify(1234)
   end
 end
