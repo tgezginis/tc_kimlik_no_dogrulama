28cd3504521a4d1ed01d3a7b56af7b1555c4314a
Tolga Gezginiş <tolga@gezginis.com>
2020-07-02 19:26:58 +0300

    Add SimpleCov for coverage

--- test/tc_kimlik_no_dogrulama_test.rb
+++ test/tc_kimlik_no_dogrulama_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
+require_relative 'test_helper'
 require 'tc_kimlik_no_dogrulama'
 
 class TcKimlikNoDogrulamaTest < Minitest::Test
--- test/validations/length_test.rb
+++ test/validations/length_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
+require_relative '../test_helper'
 require 'tc_kimlik_no_dogrulama/validations/length'
 
 class LengthValidatorTest < Minitest::Test
